b7f883aa2b62d07b7f6db12e3d7c5415e0453ed3
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-06 15:02:53 -0400

    Update FirstAsyncTest.scala

--- src/main/g8/src/test/scala/$organization__packaged$/FirstAsyncTest.scala
+++ src/main/g8/src/test/scala/$organization__packaged$/FirstAsyncTest.scala
@@ -1,1 +1,20 @@
 
+package com.axiom
+
+import org.scalatest._
+
+import wordspec._
+import matchers._
+
+import org.scalatest._
+import scala.concurrent.Future
+
+
+class FirstAsyncTest extends wordspec.AsyncWordSpec with should.Matchers{
+  "this" should {
+    "work" in {
+      val fut = Future(3)
+      fut.map(_ + 1).map(_ should be(4))
+    }
+  }
+}
