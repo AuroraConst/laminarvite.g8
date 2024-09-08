bf69c6a95bc97fe9ca8b181aa148e4a9a2ed4ece
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-08 16:11:24 -0400

    Update $classname$.scala

--- src/main/g8/src/main/scala/$organization__packaged$/$classname$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$classname$.scala
@@ -15,8 +15,39 @@ def $classname$(): Unit =
 end $classname$
 
 object Main:
-  val model = new Model
-  import model.*
+
+  val zipVar = Var("")
+  form(
+    appStyles,
+    onSubmit
+      .preventDefault
+      .mapTo(zipVar.now()) --> (zip => dom.window.alert(zip)),
+    p(
+      label("Zip code: "),
+      input(
+        placeholder("12345"),
+        maxLength(5), // HTML can help block some undesired input
+        controlled(
+          value <-- zipVar,
+          onInput.mapToValue.filter(_.forall(Character.isDigit)) --> zipVar
+        )
+      ),
+      button(
+        typ("button"), // HTML buttons are of type "submit" by default
+        "Set SF zip code",
+        onClick.mapTo("94110") --> zipVar
+      )
+    ),
+    p(
+      "Your zip code: ",
+      text <-- zipVar
+    ),
+    // Using the form element's onSubmit in this example,
+    // but you could also respond on button click if you
+    // don't want a form element
+    button(typ("submit"), "Submit")
+  )
+
 
   def appElement(): Element =
     div(
