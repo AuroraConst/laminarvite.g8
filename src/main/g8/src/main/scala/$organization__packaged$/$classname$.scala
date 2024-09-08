436de47b6044bbb781e5527d7d7b9933cae46de9
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-08 16:44:46 -0400

    Update $classname$.scala

--- src/main/g8/src/main/scala/$organization__packaged$/$classname$.scala
+++ src/main/g8/src/main/scala/$organization__packaged$/$classname$.scala
@@ -1,60 +1,30 @@
 package $organization$
 
-import scala.scalajs.js
-import scala.scalajs.js.annotation.*
+case class FormState(
+  city: String = "",
+  zip: String = "",
+  showErrors: Boolean = false
+) {
 
-import com.raquo.laminar.api.L.{*, given}
-import org.scalajs.dom
+  def hasErrors: Boolean = cityError.nonEmpty || zipError.nonEmpty
 
-@main
-def $classname$(): Unit =
-  renderOnDomContentLoaded(
-    dom.document.getElementById("app"),
-    Main.appElement()
-  )
-end $classname$
+  def cityError: Option[String] = {
+    if (city.nonEmpty) {
+      None
+    } else {
+      Some("City must not be empty.")
+    }
+  }
 
-object Main:
+  def zipError: Option[String] = {
+    if (zip.forall(Character.isDigit) && zip.length == 5) {
+      None
+    } else {
+      Some("Zip code must consist of 5 digits.")
+    }
+  }
 
-  val zipVar = Var("")
-  def controlledform = form(
-    appStyles,
-    onSubmit
-      .preventDefault
-      .mapTo(zipVar.now()) --> (zip => dom.window.alert(zip)),
-    p(
-      label("Zip code: "),
-      input(
-        placeholder("12345"),
-        maxLength(5), // HTML can help block some undesired input
-        controlled(
-          value <-- zipVar,
-          onInput.mapToValue.filter(_.forall(Character.isDigit)) --> zipVar
-        )
-      ),
-      button(
-        typ("button"), // HTML buttons are of type "submit" by default
-        "Set SF zip code",
-        onClick.mapTo("94110") --> zipVar
-      )
-    ),
-    p(
-      "Your zip code: ",
-      text <-- zipVar
-    ),
-    // Using the form element's onSubmit in this example,
-    // but you could also respond on button click if you
-    // don't want a form element
-    button(typ("submit"), "Submit")
-  )
-
-
-  def appElement(): Element =
-    div(
-      controlledform,
-      // renderDataTable(),
-    )
-  end appElement
-
-
-end Main
+  def displayError(error: FormState => Option[String]): Option[String] = {
+    error(this).filter(_ => showErrors)
+  }
+}
