084d4c985188cd7698c8cd0a8d92761a1e40cd91
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-09 08:08:32 -0400

    Update DependencyVersions.scala

--- src/main/g8/project/DependencyVersions.scala
+++ src/main/g8/project/DependencyVersions.scala
@@ -5,6 +5,7 @@ object DependencyVersions {
   val laminar           = "17.1.0"
   val laminext          = "0.17.0"
   val zioJson           = "0.7.3"
+  val borerjson         = "1.14.0"
   val betterfiles       = "3.9.2"
   val scalatest         = "3.2.19"
   val waypoint          = "8.0.1"
