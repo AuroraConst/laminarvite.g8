33c9cab659d0eee8759a4d0780374fded037b1df
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-08 20:09:06 -0400

    Update DependencyVersions.scala

--- src/main/g8/project/DependencyVersions.scala
+++ src/main/g8/project/DependencyVersions.scala
@@ -4,7 +4,7 @@ object DependencyVersions {
   val scalajsdom        = "2.8.0"
   val laminar           = "17.1.0"
   val laminext          = "0.17.0"
-  val zioJson           = "0.6.2"
+  val zioJson           = "0.7.3"
   val betterfiles       = "3.9.2"
   val scalatest         = "3.2.19"
   val waypoint          ="8.0.1"
