e34e6b43f362a99e7a4a95ca2e5269ecce605c25
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-10 18:12:06 -0400

    Update

--- src/main/g8/client/src/main/scala/$organization__packaged$/Main.scala
+++ src/main/g8/client/src/main/scala/$organization__packaged$/Main.scala
@@ -8,7 +8,6 @@ import org.scalajs.dom
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
-
 object Main {
   @main def entrypoint(): Unit = 
     println("Hello, world!")
@@ -17,24 +16,6 @@ object Main {
     println("-- Scala.js app start --")
 
 
-    dom.document.querySelector("#app").innerHTML = s"""
-     <div>
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-        <img src="javascriptLogo" class="logo vanilla" alt="JavaScript logo" />
-      </a>
-      <h1>Hello Scala.js!</h1>
-      <div class="card">
-        <button id="counter" type="button"></button>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite logo to learn more
-      </p>
-    </div>
-    """
-
-
-  
+    val element = dom.document.querySelector("#app")
+    renderOnDomContentLoaded(element,$classname$())
 }
