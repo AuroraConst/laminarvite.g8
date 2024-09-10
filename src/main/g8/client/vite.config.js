c1ee3360755d7347c5efe8e351a2cee51bf4e2cf
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-10 17:47:42 -0400

    Update vite.config.js

--- src/main/g8/client/vite.config.js
+++ src/main/g8/client/vite.config.js
@@ -2,5 +2,8 @@ import { defineConfig } from "vite";
 import scalaJSPlugin from "@scala-js/vite-plugin-scalajs";
 
 export default defineConfig({
-  plugins: [scalaJSPlugin()],
+  plugins: [scalaJSPlugin({
+    cwd: "..", // path to build.sbt
+    projectID: "client" // scala.js project name in build.sbt
+  })],
 });
