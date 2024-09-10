bfaa2e17a6d9581f4f890d410472945d8982cdc2
AuroraConst <69015147+AuroraConst@users.noreply.github.com>
2024-09-10 18:31:38 -0400

    Update Main.scala

--- src/main/g8/client/src/main/scala/$organization__packaged$/Main.scala
+++ src/main/g8/client/src/main/scala/$organization__packaged$/Main.scala
@@ -9,7 +9,12 @@ import scala.scalajs.js
 import scala.scalajs.js.annotation.JSImport
 
 object Main {
+  def consoleOut(msg: String): Unit = {
+     dom.console.log(s"%c $msg","background: #222; color: #bada55")
+  }
   @main def entrypoint(): Unit = 
+
+    consoleOut ("Hello, world from console!!!!")
     println("Hello, world!")
     // Scala.js outputs to the browser dev console, not the sbt session
     // Always have the browser dev console open when developing web UIs.
